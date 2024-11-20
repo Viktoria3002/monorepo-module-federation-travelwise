c86218e408ab946bfe4a3d6b325a7c7ebb75bea7
Viktoria Dyakova <viktoria.guliants@yandex.ru>
2024-11-21 01:42:39 +0300

    init: main | webpack and monorepo module federation added

---
+++ services/shop/src/router/Router.tsx
@@ -0,0 +1,37 @@
+import { App } from '@/components/App/App';
+import { Shop } from '@/pages/Shop';
+import { Suspense } from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import { UserCard } from '@packages/shared/src/components/UserCard';
+
+export const routes = [
+  {
+    path: '/shop',
+    element: <App />,
+    children: [
+      {
+        path: '/shop/main',
+        element: (
+          <Suspense fallback={'Loading...'}>
+            <Shop />
+          </Suspense>
+        ),
+      },
+      {
+        path: '/shop/second',
+        element: (
+          <Suspense fallback={'Loading...'}>
+            <div style={{ color: 'red' }}>
+              <h1>second page</h1>
+              <UserCard username={'FROM SHOP'} />
+            </div>
+          </Suspense>
+        ),
+      },
+    ],
+  },
+];
+
+export const router = createBrowserRouter(routes);
+
+export default routes;
